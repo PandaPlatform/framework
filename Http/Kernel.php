09facb3bc1bd1059011ef0a18c23a88355d8ad62
Ioannis Papikas <papikas.ioan@gmail.com>
2017-06-17 14:44:56 +0300

    Squashed 'src/Panda/Contracts/' content from commit b335f3a
    git-subtree-dir: src/Panda/Contracts
    git-subtree-split: b335f3accf81df2d606af230a8970ea97d0dd98d

---
+++ Http/Kernel.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the Panda Contracts Package.
+ *
+ * (c) Ioannis Papikas <papikas.ioan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Panda\Contracts\Http;
+
+use Panda\Contracts\Bootstrap\Bootstrapper;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Interface Kernel
+ * @package Panda\Contracts\Http
+ */
+interface Kernel extends Bootstrapper
+{
+    /**
+     * Handle an incoming HTTP request.
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function handle(Request $request);
+
+    /**
+     * Perform any final actions for the request lifecycle.
+     *
+     * @param Request  $request
+     * @param Response $response
+     */
+    public function terminate(Request $request, Response $response);
+}
