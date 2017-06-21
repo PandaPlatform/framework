1ab57f99c9f5a09b19efd309995e3540baebe6c1
Ioannis Papikas <papikas.ioan@gmail.com>
2017-06-22 02:09:28 +0300

    [Framework] Add RoutesConfigurationHandler
    This handler is a special handler that focuses on routes config.
    It exposes specific values for routes and it can be used
    directly by consumers instead of hiding where the routes path
    is coming from.
    It also allows mockup this functionality for tests.

---
+++ src/Panda/Support/Configuration/Handlers/RoutesConfigurationHandler.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the Panda framework.
+ *
+ * (c) Ioannis Papikas <papikas.ioan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Panda\Support\Configuration;
+
+/**
+ * Class MainConfigurationHandler
+ * @package Panda\Support\Configuration
+ */
+class RoutesConfigurationHandler extends SharedConfigurationHandler
+{
+    /**
+     * @param string $basePath
+     *
+     * @return string
+     */
+    public function getRoutesPath($basePath = null)
+    {
+        $routes = $this->get('paths.routes');
+        if (empty($routes)) {
+            // Fallback to default
+            return $basePath . DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR . 'routes' . DIRECTORY_SEPARATOR . 'main.php';
+        }
+
+        return $basePath . DIRECTORY_SEPARATOR . $routes['base_dir'] . DIRECTORY_SEPARATOR . $routes['base_file'];
+    }
+}
