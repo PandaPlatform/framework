3f943d7f56f10a30720e89cd86c0350adbd84c3c
Ioannis Papikas <papikas.ioan@gmail.com>
2017-06-22 13:44:40 +0300

    [Framework] Add BootstrapRegistry container

--- src/Panda/Foundation/Bootstrap/BootstrapRegistry.php
+++ src/Panda/Foundation/Bootstrap/BootstrapRegistry.php
@@ -14,9 +14,58 @@ namespace Panda\Foundation\Bootstrap;
 use Panda\Registry\Registry;
 
 /**
- * Class BoostrapRegistry
+ * Class BootstrapRegistry
  * @package Panda\Foundation\Bootstrap
  */
 class BootstrapRegistry extends Registry
 {
+    const CONTAINER = 'bootstrap';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get($key, $default = null)
+    {
+        // Prefix key
+        $key = self::CONTAINER . '.' . $key;
+
+        return parent::get($key, $default);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function set($key, $value)
+    {
+        // Prefix key
+        $key = self::CONTAINER . '.' . $key;
+
+        return parent::set($key, $value);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRegistry(): array
+    {
+        $registry = parent::getRegistry();
+
+
+        return $registry[self::CONTAINER];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setRegistry(array $registry)
+    {
+        // Get registry
+        $thisRegistry = $this->getRegistry();
+
+        // Set config in registry
+        $thisRegistry[self::CONTAINER] = $registry;
+
+        // Set registry back
+        parent::setRegistry($thisRegistry);
+    }
 }
