1001aee3023eb9f4e8be65205173253fc6288ba9
Ioannis Papikas <papikas.ioan@gmail.com>
2017-06-22 03:29:57 +0300

    [Framework] Create Bootstrap registry

---
+++ src/Panda/Foundation/Bootstrap/BoostrapRegistry.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the Panda framework.
+ *
+ * (c) Ioannis Papikas <papikas.ioan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Panda\Foundation\Bootstrap;
+
+use Panda\Foundation\Registry\Registry;
+
+/**
+ * Class BoostrapRegistry
+ * @package Panda\Foundation\Bootstrap
+ */
+class BoostrapRegistry extends Registry
+{
+}
