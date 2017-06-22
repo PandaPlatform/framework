f3974e4901e4778658fb31b0beafc47d65d2638c
Ioannis Papikas <papikas.ioan@gmail.com>
2017-06-22 11:18:08 +0300

    [Config] Create ConfigurationHandler
    Insert ConfigurationHandler from Contracts. Will be removed later.
    Add panda/registry composer dependency.

---
+++ src/Panda/Config/ConfigurationHandler.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the Panda Config Package.
+ *
+ * (c) Ioannis Papikas <papikas.ioan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Panda\Config;
+
+use Panda\Registry\RegistryInterface;
+
+/**
+ * Interface ConfigurationHandler
+ * @package Panda\Config
+ */
+interface ConfigurationHandler extends RegistryInterface
+{
+    /**
+     * Set the entire configuration array.
+     *
+     * @param array $config
+     */
+    public function setConfig(array $config);
+}
