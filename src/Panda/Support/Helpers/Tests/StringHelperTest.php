88cb4e22b1b3da56a778fd88234721e5dfe6f774
Ioannis Papikas <papikas.ioan@gmail.com>
2017-06-23 01:51:40 +0300

    [Helpers] Add docblock signatures in Tests

--- src/Panda/Support/Helpers/Tests/StringHelperTest.php
+++ src/Panda/Support/Helpers/Tests/StringHelperTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Panda Helpers Package.
+ *
+ * (c) Ioannis Papikas <papikas.ioan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Panda\Support\Helpers\Tests;
 
 use Panda\Support\Helpers\StringHelper;
