504ae93c90820dbf383b6004fec28f8a3e64f89c
Ioannis Papikas <papikas.ioan@gmail.com>
2017-06-22 11:51:59 +0300

    [Config] Add Configuration and SharedConfiguration
    Both implementations extend Registry and SharedRegistry
    accordingly to support the same logic.

---
+++ src/Panda/Config/SharedConfiguration.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of the Panda Config Package.
+ *
+ * (c) Ioannis Papikas <papikas.ioan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Panda\Config;
+
+use Panda\Registry\SharedRegistry;
+
+/**
+ * Class SharedConfiguration
+ * @package Panda\Config
+ */
+class SharedConfiguration extends SharedRegistry implements ConfigurationHandler
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function get($key, $default = null)
+    {
+        // Prefix key
+        $key = 'config.' . $key;
+
+        return parent::get($key, $default);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function set($key, $value)
+    {
+        // Prefix key
+        $key = 'config.' . $key;
+
+        return parent::set($key, $value);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRegistry(): array
+    {
+        $registry = parent::getRegistry();
+
+        return $registry['config'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setRegistry(array $registry)
+    {
+        // Get registry
+        $thisRegistry = $this->getRegistry();
+
+        // Set config in registry
+        $thisRegistry['config'] = $registry;
+
+        // Set registry back
+        parent::setRegistry($thisRegistry);
+    }
+}
