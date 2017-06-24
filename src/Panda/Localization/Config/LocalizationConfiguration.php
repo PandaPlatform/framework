56838b54e6e0040d89d144e4b33794b0de257e9a
Ioannis Papikas <papikas.ioan@gmail.com>
2017-06-24 19:08:48 +0300

    [Localization] Add LocalizationConfiguration, SharedConfiguration

---
+++ src/Panda/Localization/Config/LocalizationConfiguration.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the Panda framework.
+ *
+ * (c) Ioannis Papikas <papikas.ioan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Panda\Localization\Config;
+
+use Panda\Config\SharedConfiguration;
+
+/**
+ * Class LocalizationConfiguration
+ * @package Panda\Localization\Config
+ */
+class LocalizationConfiguration extends SharedConfiguration
+{
+    /**
+     * @param string $basePath
+     *
+     * @return string
+     */
+    public function getTranslationsPath($basePath = null)
+    {
+        return $basePath . DIRECTORY_SEPARATOR . $this->get('localization.base_dir');
+    }
+}
