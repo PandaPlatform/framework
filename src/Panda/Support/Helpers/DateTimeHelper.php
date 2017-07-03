8de7696fd78193fdcac673510a67a0aa372d6d17
Ioannis Papikas <papikas.ioan@gmail.com>
2017-07-03 19:28:04 +0300

    [Helpers] Update file signatures

--- src/Panda/Support/Helpers/DateTimeHelper.php
+++ src/Panda/Support/Helpers/DateTimeHelper.php
@@ -1,9 +1,12 @@
 <?php
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This file is part of the Panda Helpers Package.
+ *
+ * (c) Ioannis Papikas <papikas.ioan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Panda\Support\Helpers;
