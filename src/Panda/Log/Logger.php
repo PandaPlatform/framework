9f0a060b364c778ce5b8d0f52d4053c7ab03cc6e
Ioannis Papikas <papikas.ioan@gmail.com>
2017-08-13 17:06:02 +0300

    Add missing implementation of LoggerInterface (Logger)

---
+++ src/Panda/Log/Logger.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the Panda framework.
+ *
+ * (c) Ioannis Papikas <papikas.ioan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Panda\Log;
+
+use Psr\Log\LoggerInterface;
+
+/**
+ * Class Logger
+ * @package Panda\Log
+ */
+class Logger extends \Monolog\Logger implements LoggerInterface
+{
+}
